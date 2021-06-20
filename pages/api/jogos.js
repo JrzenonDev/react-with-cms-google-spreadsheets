3433997ebedfc7a20dc2bd2195439f38195deefc
JrzenonDev <jrobertoonb@gmail.com>
2021-06-20 20:14:01 -0300

    chore: adicionando biblioteca para leitura e manipulação das sheets do Google, estruturando a api para retornar os dados das sheets

--- pages/api/jogos.js
+++ pages/api/jogos.js
@@ -1,7 +1,35 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import credencials from '../../credencials/google-sheets-api.json';
 
-export default function(req, res) {
+export default async function(req, res) {
+
+  const doc = new GoogleSpreadsheet('12iNKT-73SjLmD_nTkmaoIXqLB90ig_Uowjg4wiBSFE8');
+
+  // initialize - Auth
+  await doc.useServiceAccountAuth({
+    client_email: credencials.client_email,
+    private_key: credencials.private_key,
+  });
+
+  await doc.loadInfo(); // carrega as infos da planilha
+  console.log('Título da planilha', doc.title);
+
+  const sheet = doc.sheetsByIndex[0]; // pega a planilha
+  const rows = await sheet.getRows(); // pega as linhas
+
+  const jogos = rows.map(({ nome, imagem_capa }) => {
+    return {
+      nome,
+      imagem_capa
+    };
+  });
+
+  console.log(jogos);
+
   res.send({
-    hello: "World"
+    title: doc.title,
+    totalJogos: sheet.rowCount,
+
+    jogos
   });
 }
